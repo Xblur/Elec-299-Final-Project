38e575bd8e97232aab776beab1cb9950ac8d097f
mhh16 <40608809+mhh16@users.noreply.github.com>
2019-03-27 16:21:24 -0400

    update to line timer

--- lineTimer/lineTimer.ino
+++ lineTimer/lineTimer.ino
@@ -1,5 +1,5 @@
  //put this snippet into forward function. this function compares current reading to previous reading
- 
+ unsigned long lastInter = 0; // Important to keep as unsigned long 
  if((lVal > thresh) && (cVal > thresh) && (rVal > thresh) && (plVal > thresh) && (pcVal > thresh) && (prVal > thresh)){ //At an intersection. Increment intersection counter
       if((millis()-lastInter)>200){
         Serial.println("INTERSECTION");
